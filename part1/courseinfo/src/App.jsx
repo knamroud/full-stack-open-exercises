53daabcfbb6e3a46087931687ab3bea7895fd6a1
Kirelos Namroud <namroudkirelos04@proton.me>
2025-10-30 19:36:39 +0100

    Add submission for exercises 1.4 and 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,15 +3,32 @@ import Content from "./Content"
 import Total from "./Total"
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {name: 'Fundamentals of React', exercises: 10}
-  const part2 = {name: 'Using props to pass data', exercises: 7}
-  const part3 = {name: 'State of a component', exercises: 14}
-  const total_exercises = part1.exercises + part2.exercises + part3.exercises
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        name: 'Fundamentals of React', 
+        exercises: 10
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data', 
+        exercises: 7
+      },
+      {
+        id: 3,
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
+  let total_exercises = 0;
+  course.parts.forEach(part => total_exercises += part.exercises)
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
       <Total total={total_exercises}/>
     </div>
   )
--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -8,12 +8,10 @@ const Part = ({name, exercises}) => {
     )
 }
 
-const Content = ({part1, part2, part3}) => {
+const Content = ({parts}) => {
     return (
         <>
-          <Part name={part1.name} exercises={part1.exercises}/>
-          <Part name={part2.name} exercises={part2.exercises}/>
-          <Part name={part3.name} exercises={part3.exercises}/>
+          {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises}/>)}
         </>
     )
 }
